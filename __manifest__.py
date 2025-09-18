3581e42c992045747380d47eb10ddda14dad8c5a
David Cancio Reyes <d_cancio.com>
2025-09-18 11:51:29 -0600

    se pone css para pintar de naranja los servicios en las lineas de orden y poner automaticamente SELECCIONAR en la columna de mechanic_id

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,15 @@
+# -*- coding: utf-8 -*-
+# ╔══════════════════════════════════════════════════════════════════╗
+# ║  DCR INFORMATIC SERVICES SAS DE CV                               ║
+# ║  Web: https://www.dcrsoluciones.com                              ║
+# ║  Contacto: info@dcrsoluciones.com                                ║
+# ║                                                                  ║
+# ║  Este módulo está bajo licencia (LGPLv3).                        ║
+# ║  Licencia completa: https://www.gnu.org/licenses/lgpl-3.0.html   ║
+# ╚══════════════════════════════════════════════════════════════════╝
 {
     'name': 'CRM Commission',
-    'version': '16.0.1.0.0',
+    'version': '16.0.1.0.1',
     'summary': 'Permite asignar comisión a los vendedores del CRM',
     'depends': ['web', 'crm', 'sale', 'hr'],   # <-- agrega 'web'
     'data': [
