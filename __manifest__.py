8fd582bfadb0f220e48d13a9951fa4bbb6728457
David Cancio Reyes <d_cancio.com>
2025-10-08 10:27:34 -0600

    El usuario escribe Margin % → recalculamos price_unit usando el costo (purchase_price) como base. Si el usuario cambia el precio manualmente, recalculamos el Margin % para que la celda muestre el % real. No tocamos el compute del margin_percent original (que es sólo lectura); simplemente lo sustituimos en la vista por nuestro campo editable. 5

--- __manifest__.py
+++ __manifest__.py
@@ -11,7 +11,7 @@
     'name': 'CRM Commission',
     'version': '16.0.1.0.1',
     'summary': 'Permite asignar comisión a los vendedores del CRM',
-    'depends': ['web', 'crm', 'sale', 'hr','sale_management', 'sale_margin'],   # <-- agrega 'web'
+    'depends': ['web', 'crm', 'sale', 'hr','sale_management', 'sale_margin'],
     'data': [
         'security/mechanic_commission_groups.xml',
         'security/ir.model.access.csv',
