67df20ae3f7beb1af0fd6374f623e641186846ed
David Cancio Reyes <d_cancio.com>
2026-07-23 12:16:48 -0600

    ocultacion de campos

--- __manifest__.py
+++ __manifest__.py
@@ -14,7 +14,7 @@
     'name': 'CRM Commission',
     'version': '16.0.1.0.1',
     'summary': 'Permite asignar comisión a los vendedores del CRM',
-    'depends': ['web', 'crm', 'sale', 'hr', 'workshop_mechanic'],
+    'depends': ['web', 'crm', 'sale', 'sale_stock', 'hr', 'workshop_mechanic'],
     'data': [
         'security/mechanic_commission_groups.xml',
         'security/ir.model.access.csv',
